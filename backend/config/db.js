d18986ab44c8e4f88160175134d98c60f82bf0bd
Maheswaran <maheswaran2220@gmail.com>
2025-01-05 01:08:34 +0530

    changes in backend files

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,20 +1,5 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
-const connectDB = async () => {
-    try {
-        const mongoURI = process.env.MONGO_URI;
-
-        if (!mongoURI) {
-            throw new Error('MongoDB URI is not defined in the environment variables.');
-        }
-
-        await mongoose.connect(mongoURI); // Remove deprecated options
-
-        console.log('DB Connected');
-    } catch (error) {
-        console.error('Error connecting to DB:', error.message);
-        process.exit(1); // Exit process with failure
-    }
-};
-
-export default connectDB;
+ export const connectDB = async () =>{
+    await mongoose.connect('mongodb+srv://Maheswaran:Mahes2203@cluster0.rvy5kal.mongodb.net/stock_management').then(()=>console.log("DB Connected"));
+ }
